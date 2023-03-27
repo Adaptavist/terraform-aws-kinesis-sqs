ef333310b3dd0c70d1b87b4e8c7609ddf22fbe0f
Johnson Howard <jhoward@adaptavist.com>
2023-03-27 16:07:29 +0100

    Fully Working

---
+++ kinesis_to_sqs/variables.tf
@@ -0,0 +1,35 @@
+variable "product" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+variable "record_type" {
+  type = string
+}
+
+variable "data_primary_key" {
+  type = string
+}
+
+variable "process_record_dir" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "stage_type" {
+  type = string
+}
+
+variable "stream_name" {
+  type = string
+}
