d155c1260d81018eb8a6a0059f50209d2198dee9
Jeremy Keelty <jkeelty@adaptavist.com>
2024-04-08 17:22:37 +0100

    add explicit sqs region and code formatting

--- add_record_to_sqs/app.py
+++ add_record_to_sqs/app.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import boto3
+from boto3.config import Config
 import base64
 import json
 from redis import Redis
@@ -9,23 +10,95 @@ import sys
 import uuid
 
 # =============SET ENVIRONMENT VARIABLES=======================
-sqs = boto3.client('sqs')
-queue_url = os.environ.get('SQS_QUEUE_URL')
-is_fifo_queue = os.environ.get('IS_FIFO_QUEUE')
+SQS_REGION = os.environ.get('SQS_REGION')
+QUEUE_URL = os.environ.get('SQS_QUEUE_URL')
+IS_FIFO_QUEUE = os.environ.get('IS_FIFO_QUEUE')
 # ==============================================================
-data_primary_key = os.environ.get('DATA_PRIMARY_KEY', '')
-redis_key = os.environ.get('REDIS_HASH_KEY', '')
-host=os.environ.get('HOST','')
-redis = Redis(host=host, port=6379)
-path_value_filter = os.environ.get('PATH_VALUE_FILTER','')
+DATA_PRIMARY_KEY = os.environ.get('DATA_PRIMARY_KEY', '')
+REDIS_KEY = os.environ.get('REDIS_HASH_KEY', '')
+HOST = os.environ.get('HOST','')
+PORT=6379
+PATH_VALUE_FILTER = os.environ.get('PATH_VALUE_FILTER','')
 # =============================================================
 # =============SET LOGGING=====================================
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.info('Loading function')
-if queue_url:
-    logger.info("Queue url : " + queue_url)
+if QUEUE_URL:
+    logger.info("Queue url : %s", QUEUE_URL)
 # =============================================================
+
+class SqsUtils:
+    """
+    Contains methods for interacting with boto3 SQS client and the redis client
+    """
+    def __init__(self):
+        boto3.setup_default_session(region_name=SQS_REGION)
+        self._client = boto3.client('sqs')
+        self._redis = Redis(host=HOST, port=PORT)
+
+    def data_to_redis_to_sqs(self, payload: dict) -> None:
+        """
+            Takes in the processed data from the lambda_handler,
+            creates a unique hash key of the record which determines whether it
+            should be added to the Redis cluster and then sent to the SQS queue
+        
+            Parameters:
+                payload (dict): The record to be sent
+        """
+
+        hash_key = create_hash_key(data=payload, key=REDIS_KEY)
+        try:
+            if self._redis.hsetnx('records', hash_key, json.dumps(payload)):
+                logger.info("New record with hash key '%s' added to hash 'records'", hash_key)
+                logger.info('Sending %s to SQS', payload)
+
+                # Send record to SQS queue
+                message_body = json.dumps(payload)
+                self.send_to_sqs(payload, message_body)
+            else:
+                logger.info("Record with hash key '%s' already exists in hash 'records'", hash_key)
+        except Exception as e:
+            logger.info('Problem sending data to the redis cluster / SQS %s', e)
+
+    def send_to_sqs(self, data: dict, message_body: str) -> None:
+        """
+            Takes the data proccessed from the lambda_handler and sends it to the SQS queue
+        
+            Parameters:
+                data (dict): The payload to be sent to SQS
+                message_body (str): Contents of the payload
+                data_base_64: 
+        """
+
+        # If redis is used take a UUID else use a HASH
+        if self._redis.connection_pool.connection_kwargs['host']:
+            message_deduplication_id = str(uuid.uuid4())
+        else:
+            # Generate the hash-based MessageDeduplicationId
+            message_deduplication_id = hashlib.md5(
+                json.dumps(data, sort_keys=True).encode()
+            ).hexdigest()
+
+        if DATA_PRIMARY_KEY:
+            try:
+                # Call the extract_keys function to get the value for groupId from the data
+                extract_value = extract_keys(data, DATA_PRIMARY_KEY.split(','))
+                group_id = str(extract_value)
+            except Exception as e:
+                raise Exception(f'Problem occurred with data_primary_key: {e}')
+        else:
+            group_id = message_deduplication_id
+
+        self._client.send_message(
+                QueueUrl=QUEUE_URL,
+                MessageBody=message_body,
+                MessageDeduplicationId=message_deduplication_id,
+                MessageGroupId=group_id)
+
+    def redis_host(self):
+        """returns the hostname of redis"""
+        return self._redis.connection_pool.connection_kwargs['host']
 
 
 def lambda_handler(event: dict, context) -> None:
@@ -36,13 +109,14 @@ def lambda_handler(event: dict, context) -> None:
             event (dict): The kinesis event
             context: LambdaContext
     """
-    
+
     logger.info(event)
-    
+    sqs = SqsUtils()
+
     for record in event['Records']:
-        dataBase64 = record['kinesis']['data']
-        dataJson = base64.b64decode(dataBase64)
-        data = json.loads(dataJson)
+        data_base_64 = record['kinesis']['data']
+        data_json = base64.b64decode(data_base_64)
+        data = json.loads(data_json)
 
         '''
         The below enables multiple consumers to optionally leverage Redis based on configuration 
@@ -54,84 +128,17 @@ def lambda_handler(event: dict, context) -> None:
         calling replace_none_values, others should be sent directly to SQS
         '''
 
-        if redis.connection_pool.connection_kwargs['host']:
-            send_to_redis = False
-
-            if path_value_filter:
-                if data.get('path') == path_value_filter:
-                    send_to_redis = True
-            else:
-                # No path_value_filter + redis connection -> all data to go via Redis
-                send_to_redis = True
-            
-            if send_to_redis:
-                data = replace_none_values(data)
-                data_to_redis_to_sqs(payload=data)
-            else:
-                send_to_sqs(data=data, message_body=json.dumps(data))
+        if sqs.redis_host() and (data.get('path') == PATH_VALUE_FILTER or PATH_VALUE_FILTER == ""):
+            data = replace_none_values(data)
+            sqs.data_to_redis_to_sqs(payload=data)
         else:
-            send_to_sqs(data=data, message_body=json.dumps(data))
+            sqs.send_to_sqs(data=data, message_body=json.dumps(data))
 
 
-def data_to_redis_to_sqs(payload: dict) -> None:
-    """
-        Takes in the processed data from the lambda_handler,
-        creates a unique hash key of the record which determines whether it
-        should be added to the Redis cluster and then sent to the SQS queue
-    
-        Parameters:
-            payload (dict): The record to be sent
-    """
-
-    hash_key = create_hash_key(data=payload, key=redis_key)
-    try:
-        if redis.hsetnx('records', hash_key, json.dumps(payload)):
-            logger.info(f"New record with hash key '{hash_key}' added to hash 'records'")
-            logger.info(f'Sending {payload} to SQS')
-            
-            # Send record to SQS queue
-            message_body = json.dumps(payload)
-            send_to_sqs(payload, message_body)
-        else:
-            logger.info(f"Record with hash key '{hash_key}' already exists in hash 'records'")
-    except Exception as e:
-        logger.info(f'Problem sending data to the redis cluster / SQS {e}')
-
-def send_to_sqs(data: dict, message_body: str) -> None:
-    """
-        Takes the data proccessed from the lambda_handler and sends it to the SQS queue
-    
-        Parameters:
-            data (dict): The payload to be sent to SQS
-            message_body (str): Contents of the payload
-            data_base_64: 
-    """
-    
-    # If redis is used take a UUID else use a HASH
-    if redis.connection_pool.connection_kwargs['host']:
-        message_deduplication_id = str(uuid.uuid4())
-    else:
-        # Generate the hash-based MessageDeduplicationId
-        message_deduplication_id = hashlib.md5(json.dumps(data, sort_keys=True).encode()).hexdigest()
-
-    if data_primary_key:
-        try:
-            # Call the extract_keys function to get the value for groupId from the data
-            extract_value = extract_keys(data, data_primary_key.split(','))
-            groupId = str(extract_value)
-        except Exception as e:
-            raise Exception(f'Problem occurred with data_primary_key: {e}')
-    else:
-        groupId = message_deduplication_id
-
-    sqs.send_message(
-            QueueUrl=queue_url,
-            MessageBody=message_body,
-            MessageDeduplicationId=message_deduplication_id,
-            MessageGroupId=groupId)   
 
 
-def extract_keys(data:dict, keys: list = None) -> str:
+
+def extract_keys(data:dict, keys: list|None = None) -> str:
     """
     Takes in a dict object and a key list.
     Loops through the data extracting the specified key
@@ -150,14 +157,13 @@ def extract_keys(data:dict, keys: list = None) -> str:
                 if key in extract:
                     extract = extract[key]
                 else:
-                    break 
+                    return str(extract)
         else:
             return 'No key provided'
     except Exception as e:
         logger.error(f'Problem occurred extract_keys: {e}')
-    return str(extract)  
 
-def create_hash_key(data:dict, key: str = None) -> str:
+def create_hash_key(data:dict, key: str|None = None) -> str:
     """
     Takes a specified key from the env vars. Returns a hash based on either this key or the entire record
 
@@ -195,5 +201,6 @@ def replace_none_values(data: dict) -> dict:
             data[key] = ''
         elif isinstance(value, dict):
             # Recurse into nested dictionaries
-            replace_none_values(value) 
+               # Recurse into nested dictionaries
+            replace_none_values(value)
     return data
