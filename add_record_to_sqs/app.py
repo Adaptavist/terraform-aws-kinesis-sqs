ba6283bc53513b0cb1c1cef0d3625c4653694b3f
Hector Kurtyanek <hkurtyanek@adaptavist.com>
2024-06-14 11:02:46 +0100

    feat: tweak type checking logic

--- add_record_to_sqs/app.py
+++ add_record_to_sqs/app.py
@@ -123,18 +123,19 @@ def lambda_handler(event: dict, context) -> None:
         calling replace_none_values, others should be sent directly to SQS
         '''
         processed_for_redis = False
+        payload = data.get('payload')
         
         for cfg in CONFIG:
-            if sqs.redis_host() and (data.get('path') == cfg["path_value_filter"] or  cfg["path_value_filter"] == ""):
+            if sqs.redis_host() and (data.get('path') == cfg["path_value_filter"] or cfg["path_value_filter"] == ""):
                 # If payload contains a list we want to ensure each record is processed individually
-                if type(data['payload']) == list:
-                    for record in data['payload']:
+                if isinstance(payload, list):
+                    for record in payload:
                         logger.info(f'Processing list payload, record: {record}')
                         data = replace_none_values(record)
                         sqs.data_to_redis_to_sqs(payload=data, config=cfg)
                         processed_for_redis = True
                 else:
-                    logger.info(f'Processing dict payload, record: {data["payload"]}')
+                    logger.info(f'Processing non list payload, data: {data}')
                     data = replace_none_values(data)
                     sqs.data_to_redis_to_sqs(payload=data, config=cfg)
                     processed_for_redis = True
